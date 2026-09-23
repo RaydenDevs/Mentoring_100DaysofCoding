119aeff0a4b469182ae0116469cc91af868f401f
Rayhan <139787019+RaydenDevs@users.noreply.github.com>
2026-09-23 20:57:46 +0800

    Add files via upload

---
+++ HanyaLatihan/PasswordBruteForce.java
@@ -0,0 +1,20 @@
+public class PasswordBruteForce {
+    public static void main(String[] args){
+        String password = "Rayhan-PembuatSoal-ICL2026"; //anggaplah password pengguna yang ingin diretas
+        String getPassword = ""; // variabel password tebakan
+
+        // pengurutan tiap simbol dalam string password (0 ~ panjang password)
+        for (int i = 0; i < password.length(); i++) {
+            // pengecekan kesamaan simbol dari 0 ~ Character.MAX_VALUE dalam bentuk integer
+            for (int j = 0; j <= Character.MAX_VALUE; j++) {
+                // kondisi kalau simbolnya benar maka j dikonversi ke char agar berubah jadi simbol dan dimasukkan ke string
+                if ((int) password.charAt(i) == j) {
+                    getPassword += (char) j; // Konversi ASCII (Simbol)
+                }
+            }
+        }
+
+        // menampilkan hasil
+        System.out.println("Password Ditemukan: "+getPassword);
+    }
+}
---
+++ HanyaLatihan/PasswordBruteForceAnimated.java
@@ -0,0 +1,34 @@
+public class PasswordBruteForceAnimated {
+    public static void main(String[] args) throws InterruptedException{
+        String password = "WhenYhJagoNgoding";
+        String getPassword = "";
+
+        if (password.isEmpty()) {
+            System.out.println("Password tidak boleh koson!");
+            return;
+        }
+        if (!getPassword.isEmpty()) {
+            System.out.println("Variabel penangkap password sebaiknya kosong!");
+            return;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            int targetChar = password.charAt(i);
+            int maxSearchLength = (targetChar <= 126) ? 126 : Character.MAX_VALUE;
+            for (int j = 32; j <= maxSearchLength; j++) {
+                System.out.print("\rScanning: "+getPassword+(char)j);
+                if ((int) password.charAt(i) == j) {
+                    getPassword += (char)j;
+                    break;
+                }
+                Wait(10);
+            }
+        }
+        System.out.println("\nPassword Ditemukan!\nPassword: "+getPassword);
+    }
+
+    static void Wait(int ms) throws InterruptedException{
+        Thread.sleep(ms);
+        System.out.flush();
+    }
+}
